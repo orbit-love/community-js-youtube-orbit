d55bbc899507dd99c3c543b715bc5bf682989286
Kevin Lewis <kvn@lws.io>
2021-05-12 16:16:54 +0100

    CLI built

---
+++ src/bin.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const OrbitYouTube = require('./index.js')
+const args = require('yargs').argv
+
+async function main() {
+
+    if(!args.comments || !args.channel || !process.env.ORBIT_WORKSPACE_ID || !process.env.ORBIT_API_KEY || !process.env.YOUTUBE_API_KEY) {
+        return console.error(`
+        You must run this command as follows:
+        npx @orbit-love/youtube --comments --channel=id --hours=24
+
+        If --hours is not provided it will default to 24.
+
+        You must also have ORBIT_WORKSPACE_ID, ORBIT_API_KEY, & YOUTUBE_API_KEY environment variables set.
+        `)
+    }
+
+    const orbitYouTube = new OrbitYouTube()
+
+    let hours
+    if(!args.hours) hours = 24
+    else if(Number.isNaN(+args.hours)) return console.error(`${args.hours} is not a number`)
+    else hours = args.hours
+
+    const videos = await orbitYouTube.getComments({ channelId: args.channel, hours })
+    const comments = await orbitYouTube.prepareComments(videos)
+    const response = await orbitYouTube.addActivities(comments)
+
+    console.log(response) // "Added n activities to the workspace-id Orbit workspace"
+    process.exit()
+
+}
+
+main()
--- src/index.js
+++ src/index.js
@@ -14,12 +14,10 @@ class OrbitYouTube {
         if(!(orbitWorkspaceId || process.env.ORBIT_WORKSPACE_ID)) throw new Error('You must provide an Orbit Workspace ID or set an ORBIT_WORKSPACE_ID environment variable')
         if(!(orbitApiKey || process.env.ORBIT_API_KEY)) throw new Error('You must provide an Orbit API Key or set an ORBIT_API_KEY environment variable')
         if(!(ytApiKey || process.env.YOUTUBE_API_KEY)) throw new Error('You must provide a YouTube API Key or set a YOUTUBE_API_KEY environment variable')
-        if(!(ytChannelId || process.env.YOUTUBE_CHANNEL_ID)) throw new Error('You must provide a YouTube Channel ID or set a YOUTUBE_CHANNEL_ID environment variable')
         return {
             orbitWorkspaceId: orbitWorkspaceId || process.env.ORBIT_WORKSPACE_ID,
             orbitApiKey: orbitApiKey || process.env.ORBIT_API_KEY,
             ytApiKey: ytApiKey || process.env.YOUTUBE_API_KEY,
-            ytChannelId: ytChannelId || process.env.YOUTUBE_CHANNEL_ID
         }
     }
 
