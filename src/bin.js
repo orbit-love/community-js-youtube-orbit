e837ffb015df6f9f19a6809b079c73d89e05195f
Kevin Lewis <kvn@lws.io>
2021-05-13 13:41:44 +0100

    Can now use env var for channel id on cli

--- src/bin.js
+++ src/bin.js
@@ -4,15 +4,14 @@ const OrbitYouTube = require('./index.js')
 const args = require('yargs').argv
 
 async function main() {
-
-    if(!args.comments || !args.channel || !process.env.ORBIT_WORKSPACE_ID || !process.env.ORBIT_API_KEY || !process.env.YOUTUBE_API_KEY) {
+    if(!args.comments || !process.env.ORBIT_WORKSPACE_ID || !process.env.ORBIT_API_KEY || !process.env.YOUTUBE_API_KEY) {
         return console.error(`
         You must run this command as follows:
         npx @orbit-love/youtube --comments --channel=id --hours=24
 
         If --hours is not provided it will default to 24.
 
-        You must also have ORBIT_WORKSPACE_ID, ORBIT_API_KEY, & YOUTUBE_API_KEY environment variables set.
+        You must also have ORBIT_WORKSPACE_ID, ORBIT_API_KEY, & YOUTUBE_API_KEY environment variables set. You may optionally use YOUTUBE_CHANNEL_ID and omit the --channel option on the CLI.
         `)
     }
 
@@ -23,7 +22,10 @@ async function main() {
     else if(Number.isNaN(+args.hours)) return console.error(`${args.hours} is not a number`)
     else hours = args.hours
 
-    const videos = await orbitYouTube.getComments({ channelId: args.channel, hours })
+    let channelId = args.channel ? args.channel : process.env.YOUTUBE_CHANNEL_ID
+    if(!channelId) return console.error('You must use a --channel=channelId option or have a YOUTUBE_CHANNEL_ID environment variable.')
+
+    const videos = await orbitYouTube.getComments({ channelId, hours })
     console.log(`Fetched ${videos.length} comments from the provided timeframe`)
     const comments = await orbitYouTube.prepareComments(videos)
     console.log(`Comments are prepared as Orbit activities. Sending them off now...`)
